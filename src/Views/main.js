cbb847eef8ddd9dcf25290f01d121e7574da1d1f
Sanbedan Paul <sanbedan9989@gmail.com>
2024-05-03 22:33:57 +0530

    Filters Applied

--- src/Views/main.js
+++ src/Views/main.js
@@ -4,10 +4,26 @@ import JobCard from "./jobCards";
 export default function Appli() {
   const [jobData, setJobData] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [filters, setFilters] = useState({
+    minExperience: "",
+    companyName: "",
+    location: "",
+    techStack: "",
+    role: "",
+    minBasePay: "",
+    remote: false,
+  });
 
   useEffect(() => {
     fetchJobData();
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [jobData]);
 
   const fetchJobData = () => {
     const limit = 15;
@@ -37,13 +53,6 @@ export default function Appli() {
       .catch((error) => console.error(error));
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [jobData]);
-
   const handleScroll = () => {
     if (
       document.documentElement.offsetHeight -
@@ -55,156 +64,117 @@ export default function Appli() {
     }
   };
 
+  const handleFilterChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const filterValue = type === "checkbox" ? checked : value;
+    setFilters({
+      ...filters,
+      [name]: filterValue,
+    });
+  };
+
+  const filteredJobs = jobData.filter((job) => {
+    const {
+      minExperience,
+      companyName,
+      location,
+      remote,
+      techStack,
+      role,
+      minBasePay,
+    } = filters;
+    return (
+      (minExperience
+        ? parseInt(job.minExp) <= parseInt(minExperience)
+        : true) &&
+      (companyName
+        ? job.companyName.toLowerCase().includes(companyName.toLowerCase())
+        : true) &&
+      (location
+        ? job.location.toLowerCase().includes(location.toLowerCase())
+        : true) &&
+      (techStack
+        ? job.jobDetailsFromCompany
+            .toLowerCase()
+            .includes(techStack.toLowerCase())
+        : true) &&
+      (role ? job.jobRole.toLowerCase() === role.toLowerCase() : true) &&
+      (minBasePay ? parseInt(job.minJdSalary) >= parseInt(minBasePay) : true) &&
+      (remote ? job.location.toLowerCase().includes("remote") : true)
+    );
+  });
+
   return (
     <>
-      <div className="flex mt-5 px-5">
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-
-        <div className="relative w-full max-w-full mx-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-7 md:top-3 bottom-0 w-5 h-5 my-auto text-gray-400 right-3 transform transition-transform duration-200"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <select className="w-full px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2 transition-all duration-200 ease-in-out transform hover:scale-105">
-            <option value="null">None</option>
-            <option value="hr">HR</option>
-            <option value="eng">Engineering</option>
-            <option value="acc">Accounts</option>
-            <option value="sals">Sales</option>
-          </select>
-        </div>
-        <div>
+      <div className="flex mt-5 px-5 flex-wrap gap-2">
+        <input
+          type="text"
+          name="minExperience"
+          placeholder="Min Exp"
+          value={filters.minExperience}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        />
+        <input
+          type="text"
+          name="companyName"
+          placeholder="Company"
+          value={filters.companyName}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={filters.location}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        />
+        <input
+          type="text"
+          name="techStack"
+          placeholder="Tech Stack"
+          value={filters.techStack}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        />
+        <select
+          name="role"
+          value={filters.role}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        >
+          <option value="">Role</option>
+          <option value="ios">iOS</option>
+          <option value="frontend">Frontend</option>
+          <option value="backend">Backend</option>
+          <option value="android">Android</option>
+          <option value="techlead">Tech Lead</option>
+        </select>
+        <input
+          type="text"
+          name="minBasePay"
+          placeholder="Min Base Pay"
+          value={filters.minBasePay}
+          onChange={handleFilterChange}
+          className="placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        />
+        <label className="flex items-center gap-1">
           <input
-            type="text"
-            placeholder="John Doe"
-            class="block w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+            type="checkbox"
+            name="remote"
+            checked={filters.remote}
+            onChange={handleFilterChange}
+            className="mr-1"
           />
-        </div>
+          Remote
+        </label>
       </div>
-      <section className="bg-white pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px] lg:px-16 ">
+      <section className="bg-white pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px] lg:px-16">
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center gap-10">
-            {jobData.map((job) => (
+            {filteredJobs.map((job) => (
               <JobCard key={job.jdUid} job={job} />
             ))}
           </div>
